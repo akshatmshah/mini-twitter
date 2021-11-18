b4742706aaac30b0b96a965878e024cc1634e9d5
Akshat Shah <ashah1506@gmail.com@example.com>
2021-11-18 12:56:43 -0500

    Tweet feed, tweet creation, and users implemented

--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render, get_object_or_404
+from django.utils import timezone
+from .models import Tweet, Profile
+from django.views import generic
+from main.forms import TweetForm
 
 def splash(request):
     return render(request, "home.html", {})
@@ -26,8 +30,27 @@ def signup_(request):
     elif request.method == "GET":
         return render(request, 'signup.html', {})
 
-def feed(request):
-    return render(request, "feed.html", {})
+def get_user_profile(request, username):
+    user = get_object_or_404(User, pk=username)
+    prof = Profile.objects.get_or_create(user=user)
+    return render(request, "profile.html", {"profile": prof})
+
+class Feed(generic.ListView):
+    template_name = 'feed.html'
+    context_object_name = 'tweets'
+    def get_queryset(self):
+        return Tweet.objects.filter(time__lte=timezone.now()).order_by('-time')
+
+def create_post(request):
+    form = TweetForm
+    if request.method == 'POST':
+        postf = TweetForm(request.POST)
+        if postf.is_valid():
+            profile = postf.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('/feed')
+    return render(request, 'post.html', {'form': form})
 
 
 def logout_(request):
