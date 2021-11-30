67f06286b919c2d60db697e68f082ff9ecd0dfdd
Akshat Shah <ashah1506@gmail.com>
2021-11-30 12:36:57 -0500

    added like and unlike feature

--- main/models.py
+++ main/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import related
 from taggit.managers import TaggableManager
 
 # Create your models here.
 class Tweet(models.Model):
-    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     description = models.TextField()
     time = models.DateTimeField(auto_now_add=True, null = True)
     tags = TaggableManager()
+    like = models.ManyToManyField(User, related_name = 'tweet')
 
     class Meta:
         app_label = "main"
